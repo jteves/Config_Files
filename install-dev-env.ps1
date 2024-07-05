a0d17c513436a7bbc9e398df7fe262c8f7a747ce
Jacob <jacobteves@gmail.com>
2024-07-04 20:59:20 -0700

    Update install-dev-env.ps1
    Added link to ms profile and simplifying shell font/prompt customization. Added other shell inits

--- install-dev-env.ps1
+++ install-dev-env.ps1
@@ -3,8 +3,11 @@ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 
 # Install winget with the following command or the PS function.
 # Note winget should already be installed on most windows machines
-start ms-appinstaller:?source=https://aka.ms/getwinget
+if (!(Get-Command winget)) {
+  start ms-appinstaller:?source=https://aka.ms/getwinget
+}
 
+# MS software
 winget install --id Microsoft.VisualStudioCode
 winget install --id Microsoft.VisualStudio.2022.Enterprise
 winget install --id Microsoft.WindowsTerminal
@@ -20,6 +23,7 @@ winget install -h --id Python.Python.3
 winget install -h --id OpenJS.Nodejs
 winget install -h --id Docker.DockerDesktop
 winget install -h --id GoLang.Go
+winget install -h ScooterSoftware.BeyondCompare4
 
 # Credential provider for the dotnet cli
 iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) }"
@@ -27,9 +31,16 @@ iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) }"
 # Wsl setup (http://aka.ms/wsl)
 wsl --install
 
-#Vim
+# My software
 winget install -h --id vim.vim
-Copy-Item Vim-Config/.vimrc $HOME
+Copy-Item $PSScriptRoot/Vim-Config/.vimrc $HOME
+
+# Link powershell profile to MS onedrive profile (pwsh)
+$msProfile = 'C:\Users\jateves\OneDrive - Microsoft\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
+if (!(Test-Path $PROFILE)) {
+  mkdir -Path (Split-Path -Parent $profile) -ErrorAction SilentlyContinue
+  New-Item $PROFILE -Value ". '$msProfile'"
+}
 
 # Font and Prompt Customization
 winget install JanDeDobbeleer.OhMyPosh -s winget
@@ -37,12 +48,21 @@ $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
 oh-my-posh font install Meslo
 Write-Warning "Add 'MesloLGM Nerd Font' to: `n-VSCode: Terminal.Integrated.FontFamily `n-Windows Terminal: > Settings > Defaults > Appearance > Font face"
 Install-Module -Name Terminal-Icons -Repository PSGallery
-$pContent = Get-Content $PROFILE 
-if (!([string]$pContent).Contains("oh-my-posh")) {
-  Add-Content -Path $PROFILE -Value "`noh-my-posh init pwsh | Invoke-Expression`n"
+if (!(Test-Path $msProfile)) {
+  $pContent = Get-Content $PROFILE
+  if (!([string]$pContent).Contains("oh-my-posh")) {
+    Add-Content -Path $PROFILE -Value "`noh-my-posh init pwsh | Invoke-Expression`n"
+  }
+  if (!([string]$pContent).Contains("Terminal-Icons")) {
+    Add-Content -Path $PROFILE -Value "Import-Module -Name Terminal-Icons"
+  }
 }
-if (!([string]$pContent).Contains("Terminal-Icons")) {
-  Add-Content -Path $PROFILE -Value "Import-Module -Name Terminal-Icons"
-}
+
+# path env variables
+$env:Path += ";%USERPROFILE%\tools"
+
+
+Update-Module
+Update-Help
 
 . $PROFILE
